bfb2be7739254afe0cf1af6a599d9f855cd57a34
BinaryNikita <169074248+BinaryNikita@users.noreply.github.com>
2024-10-18 11:33:15 +0000

    "Added new Files"

--- Hello.js
+++ Hello.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const id = 101;
+let name = "Nikita Vishnoi";
+let email = "nikita@gmail.com";
+ password = 98765;
+
+console.table([id, name, email, password]);
+console.log(typeof password);
+
