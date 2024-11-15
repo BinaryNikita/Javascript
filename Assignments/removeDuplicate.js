c789fb0c10cda4b4a513eb136049c698c66f7c85
BinaryNikita <169074248+BinaryNikita@users.noreply.github.com>
2024-11-15 01:49:26 +0000

    remove Duplicates from array

---
+++ Assignments/removeDuplicate.js
@@ -0,0 +1,20 @@
+let arr = [1, 2, 22, 3, 2, 3, 9];
+let newArray = [];
+// arr = Array.from(new Set(arr));
+
+for (let i = 0; i < arr.length; i++) {
+
+    let isPresent = false;
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] == arr[j]) {
+isPresent = true;
+
+    }
+  }
+
+  if(!isPresent){
+    newArray.push(arr[i]);
+  }
+}
+
+console.log(newArray);
