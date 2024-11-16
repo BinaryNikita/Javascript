7b61c7e8306bbfcb4fff1c520b5edbd43766b7af
BinaryNikita <169074248+BinaryNikita@users.noreply.github.com>
2024-11-16 02:20:20 +0000

    Array to Binary Array Program

---
+++ ArrayAndStrings/removeAElement.js
@@ -0,0 +1,3 @@
+// Given an array and a key, the task is to remove all occurrences of the specified key from the array in Java. Examples:
+// Input: array = { 3, 9, 2, 3, 1, 7, 2, 3, 5 }, key = 3
+// Output: [9, 2, 1, 7, 2, 5]
