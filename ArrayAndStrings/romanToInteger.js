19b25a915100213021953581b8eddecb61f8b10e
BinaryNikita <169074248+BinaryNikita@users.noreply.github.com>
2024-11-18 01:48:22 +0000

    roman to integer

--- ArrayAndStrings/romanToInteger.js
+++ ArrayAndStrings/romanToInteger.js
@@ -6,4 +6,27 @@ Input: s = "MCMXCIV"
 Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90, IV = 4.
 
+
+
 */
+const romanMap = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+};
+
+let roman = "MCMXCIV";
+let total = 0;
+
+for (let i = 0; i < roman.length; i++) {
+    const current = romanMap[roman[i]];
+    const next = romanMap[roman[i + 1]];
+
+    total += current < next ? -current : current;
+}
+
+console.log(total); 
