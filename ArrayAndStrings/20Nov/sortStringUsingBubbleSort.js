149ece563ac06ecef92643279e973360bde855e9
Nikita Vishnoi <nikita.works32@gmail.com>
2024-11-22 12:02:24 +0530

    sorting string using bubble sort

--- ArrayAndStrings/20Nov/sortStringUsingBubbleSort.js
+++ ArrayAndStrings/20Nov/sortStringUsingBubbleSort.js
@@ -1,27 +1,25 @@
 // Sorting Strings using Bubble Sort.
 
-let str = "nikita";
-
-
+let str = 'nikita';
 let arr = [];
 
-for(let i in str){
-    arr[i] = str.charAt(i);
+for (let i in str) {
+  arr[i] = str.charAt(i);
 }
 
-for(let i = 0; i < arr.length; i++){
-    for(let j  = i + 1; j < arr.length; j++){
-        if(arr[i] > arr[j]){
-            let temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] > arr[j]) {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
     }
+  }
 }
 
-str = "";
-for(let a of arr){
-    str += a;
+str = '';
+for (let a of arr) {
+  str += a;
 }
 
 console.log(str);
