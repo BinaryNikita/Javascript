007a60caf4f77e560bd66413a3195aae911c2de2
Nikita Vishnoi <nikita.works32@gmail.com>
2024-11-22 08:16:21 +0530

    sort string string using bubble sort program

---
+++ ArrayAndStrings/20Nov/sortStringUsingBubbleSort.js
@@ -0,0 +1,27 @@
+// Sorting Strings using Bubble Sort.
+
+let str = "nikita";
+
+
+let arr = [];
+
+for(let i in str){
+    arr[i] = str.charAt(i);
+}
+
+for(let i = 0; i < arr.length; i++){
+    for(let j  = i + 1; j < arr.length; j++){
+        if(arr[i] > arr[j]){
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
+
+str = "";
+for(let a of arr){
+    str += a;
+}
+
+console.log(str);
