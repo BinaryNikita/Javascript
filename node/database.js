ec84c94b2fc063363623783db5371b495af8f883
Nikita Vishnoi <nikita.works32@gmail.com>
2024-11-22 21:37:35 +0530

    crud operation using node

--- node/database.js
+++ node/database.js
@@ -3,15 +3,54 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root',
-  database: 'Tech_Solutions'
+  database: 'Tech_Solutions',
 });
 
-connection.query(`select * from Employees`, (err, result) => {
-  if(err){
-    console.log(err);
-  }
+function displayResult(query) {
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      return;
+    }
+    console.table(results);
+  });
+}
 
-  return console.table(result)
-});
+
+
+function deleteRecord(query) {
+  connection.query(query, (err, result) => {
+    if (err) {
+      console.log('Error while deleting record:', err);
+    } else {
+      console.log(`Deleted ${result.affectedRows} record(s) successfully.`);
+    }
+  });
+}
+
+deleteRecord(`DELETE FROM Employees WHERE employee_id = 2`);
+
+function updateRecord(query){
+
+  connection.query(query, (err, result) => {
+    if(err){
+      console.log('Error while updating record:', err);
+    } else {
+      console.log(`Updated ${result.affectedRows} record(s) successfully.`);
+
+    }
+  })
+}
+
+updateRecord("update Employees set first_name = 'Nikita' where employee_id = 2");
+');
 
 module.exports = connection;
+
+// connection.query(`select * from Employees where employee_id >= 5`, (err, result) => {
+//   if(err){
+//     console.log(err);
+//   }
+
+//   return console.table(result)
+// });
