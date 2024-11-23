92c9adb4f3143897714c7a9db10ce559239e06fe
Nikita Vishnoi <nikita.works32@gmail.com>
2024-11-23 19:35:22 +0530

    created a crud program using nodejs module using mysql database

--- node/database.js
+++ node/database.js
@@ -6,6 +6,16 @@ const connection = mysql.createConnection({
   database: 'Tech_Solutions',
 });
 
+function createUser(query, values) {
+  connection.query(query, values, (err, result) => {
+    if (err) {
+      console.log('Error occured while creating the user: ', err);
+    } else {
+      console.log('User created succesfully');
+    }
+  });
+}
+
 function displayResult(query) {
   connection.query(query, (err, results) => {
     if (err) {
@@ -16,34 +26,48 @@ function displayResult(query) {
   });
 }
 
-
-
 function deleteRecord(query) {
   connection.query(query, (err, result) => {
     if (err) {
       console.log('Error while deleting record:', err);
     } else {
       console.log(`Deleted ${result.affectedRows} record(s) successfully.`);
+
     }
   });
 }
 
-deleteRecord(`DELETE FROM Employees WHERE employee_id = 2`);
+deleteRecord(`DELETE FROM Employees WHERE first_name = 'Pooja'`);
 
-function updateRecord(query){
-
+function updateRecord(query) {
   connection.query(query, (err, result) => {
-    if(err){
+    if (err) {
       console.log('Error while updating record:', err);
     } else {
       console.log(`Updated ${result.affectedRows} record(s) successfully.`);
-
     }
-  })
+  });
 }
 
-updateRecord("update Employees set first_name = 'Nikita' where employee_id = 2");
-');
+//const sql = connection.format('SELECT * FROM users WHERE name = ?', ['John']);
+// console.log(sql); // Output: SELECT * FROM users WHERE name = 'John'
+
+
+// updateRecord("update Employees set first_name = 'Nikita' where employee_id = 2");
+// const sql = 'INSERT INTO employees (first_name, last_name, email, phone_number, job_title, salary, department_id) VALUES (?, ?, ?, ?, ?, ?, ?)';
+// const values = ["Nikita", "Vishnoi", "nikita@gmail.com", "9876543210", "Java Developer", 100000, 2];
+// createUser(sql, values);
+function close(){
+  connection.end((err) => {
+    if (err) {
+      console.log('Error closing the connection:', err);
+    } else {
+      console.log('Connection closed.');
+    }
+  });
+}
+
+close();
 
 module.exports = connection;
 
