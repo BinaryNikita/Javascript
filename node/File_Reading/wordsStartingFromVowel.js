535eb6d19ab9a14c23934c690bf910df566e8a07
Nikita Vishnoi <nikita.works32@gmail.com>
2024-11-25 17:22:49 +0530

    total words starting from vowels

--- node/File_Reading/wordsStartingFromVowel.js
+++ node/File_Reading/wordsStartingFromVowel.js
@@ -1,2 +1,23 @@
 const fs = require('fs');
 
+function logFileContent(filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const vowels = 'aeiouAEIOU';
+      const words = data.split(/\s+/);
+      let count = 0;
+      for (let i = 0; i < words.length; i++) {
+        if (words[i] && vowels.includes(words[i].charAt(0))) {
+          count++;
+        }
+      }
+
+      console.log('Total words starting from a vowel: ' + count);
+    }
+  });
+}
+
+
+logFileContent('node/File_Reading/file3.txt');
