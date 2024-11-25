75b9451fdf0cfb4017955d45c3f96da3b0c67f63
Nikita Vishnoi <nikita.works32@gmail.com>
2024-11-25 15:30:08 +0530

    copy data from one to another using fs module

---
+++ node/File_Reading/copyData.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+function copyData(origin, destination){
+
+    fs.readFile(origin, 'utf-8', (err, data) => {
+        if (err) {
+          console.log("Error while reading");
+        } else {
+    
+          fs.writeFile(destination, data, (err) => {
+            if(err){
+               console.log("Error while writing")
+            } else{
+                console.log("Content written sucesfully");
+            }
+          });
+        }
+      });
+
+}
+
+
+copyData("node/File_Reading/file1.txt", "node/File_Reading/file3.txt");
