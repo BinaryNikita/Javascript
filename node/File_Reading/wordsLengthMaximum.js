395249fc99cfb985322c7154d6028088d24df924
Nikita Vishnoi <nikita.works32@gmail.com>
2024-11-26 07:59:06 +0530

    word with maximum length

---
+++ node/File_Reading/wordsLengthMaximum.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+function logFileContent(filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const words = data.split(/\s+/);
+      let max = words[0];
+      for(let a of words){
+        if(a.length > max.length){
+            max = a;
+        }
+      }
+      console.log('Largest ' + max);
+    }
+  });
+}
+
+
+logFileContent('node/File_Reading/file3.txt');
